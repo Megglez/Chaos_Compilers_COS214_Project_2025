4d3b7180f2bee8fe2fb2d1974bb537c440774dc8
Megglez <megan.laisa24@gmail.com>
2025-10-26 13:58:52 +0200

    Fixed up dependencies and links

--- src/Greenhouse/AddStock.h
+++ src/Greenhouse/AddStock.h
@@ -1,18 +1,23 @@
 #ifndef ADDSTOCK_H
 #define ADDSTOCK_H
+// AddStock.h dependencies
 #include <iostream>
+#include <memory>
 #include "Command.h"
 #include "Inventory.h"
+#include "Plant.h"
 using namespace std;
 
-class AddStock : public Command {
+class AddStock : public Command
+{
 public:
 	bool isEmpty;
-	void execute(std::unique_ptr<Plant>&& plant, int quantity) override;
-	AddStock(Inventory* inv);
+	void execute(std::unique_ptr<Plant> &&plant, int quantity) override;
+	AddStock(Inventory *inv);
 	virtual ~AddStock();
+
 private:
-	Inventory* inventory;
+	Inventory *inventory;
 };
 
 #endif
