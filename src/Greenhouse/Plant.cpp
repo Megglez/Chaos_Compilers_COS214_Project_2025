0258f318665bf3fae07957b20220480e496eb0ee
Divo <u24607526@tuks.co.za>
2025-10-27 11:16:08 +0200

    reconfigured methods

--- src/Greenhouse/Plant.cpp
+++ src/Greenhouse/Plant.cpp
@@ -1,27 +1,28 @@
 #include "Plant.h"
 
+Plant::Plant(int water,int soil,int sunlight, StageOfDevelopment stage,CareStrategy* strategy)
+{
+    this->water=water;
+    this->soil=soil;
+    this->sunlight=sunlight;
+    StageOfDevelopment* stage = new StageOfDevelopment();
+    CareStrategy* strategy = new CareStrategy();
+}
+
 Plant *Plant::clone()
 {
     return nullptr;
 }
 
-void Plant::package()
-{
-}
-
 void Plant::grow()
 {
-    // TODO - implement Plant::grow
-	
+    this->grow();
+
 }
 
-void Plant::helpPlant() {
+void Plant::helpPlant() 
+{
 	// TODO - implement Plant::helpPlant
-	
-}
-
-Plant::Plant() {
-	// TODO - implement Plant::Plant
 	
 }
 
