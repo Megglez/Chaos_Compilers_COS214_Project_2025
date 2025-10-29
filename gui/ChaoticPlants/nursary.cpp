b4c4790fab9341efb68e9ec54f6ca831ea3e51ab
Megglez <megan.laisa24@gmail.com>
2025-10-29 16:30:03 +0200

    qt linked to vscode

---
+++ gui/ChaoticPlants/nursary.cpp
@@ -0,0 +1,14 @@
+#include "nursary.h"
+#include "./ui_nursary.h"
+
+Nursary::Nursary(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Nursary)
+{
+    ui->setupUi(this);
+}
+
+Nursary::~Nursary()
+{
+    delete ui;
+}
