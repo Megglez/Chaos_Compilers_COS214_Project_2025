ed71bc993e78bb7de6a633fce331e1e65a469fa5
u23544563 <u23544563@tuks.co.za>
2025-11-03 07:53:57 +0200

    I don't know

--- src/Nursery/Nursery.h
+++ src/Nursery/Nursery.h
@@ -14,6 +14,8 @@
 #include "../Greenhouse/Summer.h"
 #include "../Greenhouse/FlowerPlanter.h"
 #include "../Greenhouse/HerbPlanter.h"
+#include "../Greenhouse/TreePlanter.h"
+#include "../Greenhouse/SucculentPlanter.h"
 using namespace std;
 
 class Nursery : public QObject // <-- INHERIT FROM QObject
@@ -21,9 +23,8 @@ class Nursery : public QObject // <-- INHERIT FROM QObject
 Q_OBJECT // REQUIRED
     private :
     // Customer Management
-    vector<Customer *>
-        activeCustomers;
-    CustomerCreator *customerFactory;
+    vector<Customer*> activeCustomers;
+    CustomerCreator* customerFactory;
     int customerCount;
     int customerLimit;
 
@@ -36,6 +37,8 @@ Q_OBJECT // REQUIRED
     Inventory *inventory;
     FlowerPlanter *flowerFactory;
     HerbPlanter *herbFactory;
+    TreePlanter *treeFactory;
+    SucculentPlanter *succulentFactory;
     Seasons *currentSeason;
 
 public:
